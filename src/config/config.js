b2263152523119c47b1fb1f0f1158545dbcc4b36
koala1029 <koala1201au@gmail.com>
2025-03-26 03:02:43 +0900

    update the factory contract addresss

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const API_URL = "http://localhost:5000/";
 const CONTRACT_ADDRESS = "0xB9e09e1447e25E10A9fC35cD26510bCcE613aF57";
 const TOKEN_VAULT_FACTORY_ADDRESS =
-  "0x638344Fe6dd1c9778c93b129F1Cc26aABEE28248";
+  "0xfA532699fe6A897cAF8fce9BE8EE366A573e6d72";
 const RPC_URL = "https://sepolia.base.org";
 
 const baseChainId = "0x14a34"; // Hexadecimal for 8453 (Base Mainnet Chain ID)
